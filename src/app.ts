cd2e614b69847703105a57e1691116e208190e51
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-06-04 00:17:25 +0200

    implemetn ts config

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express, { Application, Request, Response } from "express";
 const app: Application = express();
 
+import auth from "@/routes/auth";
+import userRoutes from "@/routes/users";
+import exerciseroutes from "@/routes/exercises";
+
 import dotenv from "dotenv";
 import cors from "cors";
 
@@ -12,7 +16,9 @@ app.get("/", (req: Request, res: Response) => {
 	res.send("welcome to server");
 });
 
-const PORT: Number = 5000;
+app.use("/api/auth", auth);
+
+const PORT = 5000;
 app.listen(PORT, () => {
 	console.log(`Server is operational on port: ${PORT}`);
 });
