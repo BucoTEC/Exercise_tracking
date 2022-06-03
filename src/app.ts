16321295dbbde480ab5c88f44b2a23a71cc703cc
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-06-03 23:10:43 +0200

    add basic config

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
-import express from "express";
-const app = express();
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
 
-app.get("/", (req, res) => {
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
 	res.send("welcome to server");
 });
 
-const PORT = 5000;
+const PORT: Number = 5000;
 app.listen(PORT, () => {
 	console.log(`Server is operational on port: ${PORT}`);
 });
