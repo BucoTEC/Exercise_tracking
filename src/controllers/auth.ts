9885b2bfb04cad0f1553f48c1fbc57b078f4d1aa
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-06-03 23:47:19 +0200

    update auth ts controllers

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
 
-export const login = (req: Request, res: Response) => {
+export const login = (req: Request, res: Response): void => {
 	res.json("login route");
 };
 
-export const register = (req: Request, res: Response) => {
+export const register = (req: Request, res: Response): void => {
 	res.json("registr route");
 };
