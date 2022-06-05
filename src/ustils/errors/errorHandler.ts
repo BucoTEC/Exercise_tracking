7310031ac89a9b1f9491b577ef37d908041b8067
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-06-05 23:57:27 +0200

    fix error handler

--- src/ustils/errors/errorHandler.ts
+++ src/ustils/errors/errorHandler.ts
@@ -1,11 +1,12 @@
-import { Request, Response, ErrorRequestHandler } from "express";
+import { Request, Response, ErrorRequestHandler, NextFunction } from "express";
 
 import ResError from "./customError";
 
 const errorHandler = (
 	err: ErrorRequestHandler,
 	req: Request,
-	res: Response
+	res: Response,
+	next: NextFunction
 ) => {
 	// console.error(err.stack);
 	if (err instanceof ResError) {
