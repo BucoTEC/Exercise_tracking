6bdf6acf31f476f81f9c01b269aa5aee60851a47
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-06-15 00:06:14 +0200

    add create user functionaliti

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,16 +1,28 @@
 import { Request, Response } from "express";
 import ReqWithUser from "@/utils/types/ReqWithUser";
+import "express-async-errors";
+
+import User from "@/models/userModel";
 
 export const updateUser = (req: Request, res: Response): void => {
 	res.json("update route");
 };
 
-export const deleteUser = (req: ReqWithUser, res: Response): void => {
+export const deleteUser = async (req: ReqWithUser, res: Response) => {
 	const { userData } = req;
 	const { id } = req.params;
+
+	const user = await User.findByPk(userData?.userId);
+
+	if (!user) {
+		throw new Error("no user found");
+	}
 
 	if (parseInt(id) !== userData?.userId) {
 		throw new Error("not owner");
 	}
-	res.json({ data: req.userData?.userId, param: parseInt(req.params.id) });
+
+	await User.destroy({ where: { id: userData.userId } });
+
+	res.json("succesfly delted user");
 };
