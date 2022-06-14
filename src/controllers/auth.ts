0e6c7608e7bb4a33c51ef6b60d2057b7e6c0b78b
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-06-14 20:57:34 +0200

    fix imports on auth

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,14 +1,32 @@
 import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 import "express-async-errors";
 
 import User from "@/models/userModel";
+
+dotenv.config();
+
+const tokenSecret = process.env.JWT_SECRET || "tajna";
 
 export const login = (req: Request, res: Response) => {
 	res.json("login route");
 };
 
 export const register = async (req: Request, res: Response) => {
-	const test = User.build(req.body);
-	await test.save();
-	return res.json({ msg: "succes", newUser: test });
+	const { username, email, password } = req.body;
+
+	const hashedPassword = await bcrypt.hash(password, 12);
+
+	const newUser = User.build({ username, email, password: hashedPassword });
+	await newUser.save();
+	const token = jwt.sign(
+		{
+			userId: newUser.id,
+		},
+		tokenSecret,
+		{ expiresIn: "10m" }
+	);
+	return res.json({ msg: "succes", newUser: newUser, token });
 };
